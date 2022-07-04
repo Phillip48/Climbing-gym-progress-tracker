92f4f5d21efa0772f0a6b612ce02a0adf4256fa8
Phillip48 <phillippereira02@gmail.com>
2022-07-04 19:08:56 -0400

    Styled login page

--- client/src/components/login/index.js
+++ client/src/components/login/index.js
@@ -3,12 +3,24 @@ import React from "react";
 import '../login/style.css'
 
 const Login = () => {
-    
+
 
     return (
         <>
             <section className="hold-everything-login">
-                
+                <section className="holds-login-interaction">
+                    <div className="login-form">
+                        <h1 className="login-form-text">Login:</h1>
+                    </div>
+
+                    <div className="login-info">
+                        <h1 className="login-info-text ">Don't have an account?</h1>
+                        <p className="login-info-subtext">
+                            No worries! You can signup for free! It's quick and easy. When you do signup you'll be on the track to 
+                            logging your climbing progress!
+                        </p>
+                    </div>
+                </section>
             </section>
         </>
     )
