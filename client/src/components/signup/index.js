c136286667ae3e792bb39c23aa7c4cd5e7eab9c3
Phillip48 <phillippereira02@gmail.com>
2022-07-09 10:27:47 -0400

    Possibly added code for login and signup

--- client/src/components/signup/index.js
+++ client/src/components/signup/index.js
@@ -4,6 +4,49 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../signup/style.css'
 
 const Signup = () => {
+    // refractor and use state
+    const signupFormHandler = async (event) => {
+        event.preventDefault();
+
+        const username = document.querySelector('#username-signup').value.trim();
+        const email = document.querySelector('#email-signup').value.trim();
+        const password = document.querySelector('#password-signup').value.trim();
+
+        if (username && email && password) {
+            const response = await fetch('/api/users', {
+                method: 'POST',
+                body: JSON.stringify({ username, email, password }),
+                headers: { 'Content-Type': 'application/json' },
+            });
+
+            if (response.ok) {
+                document.location.replace('/');
+            } else {
+                alert('Failed to sign up.');
+            }
+        }
+    };
+    const loginFormHandler = async (event) => {
+        event.preventDefault();
+        console.log("Testing")
+
+        const email = document.querySelector('#email-login').value.trim();
+        const password = document.querySelector('#password-login').value.trim();
+
+        if (email && password) {
+            const response = await fetch('/api/users/login', {
+                method: 'POST',
+                body: JSON.stringify({ email, password }),
+                headers: { 'Content-Type': 'application/json' },
+            });
+
+            if (response.ok) {
+                document.location.replace('/');
+            } else {
+                alert('Failed to log in.');
+            }
+        }
+    };
     return (
         <>
             <section className="hold-everything-Signup">
