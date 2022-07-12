7b7d568bac4f7f68e073f15aa9cfeb07bf408e53
Phillip48 <phillippereira02@gmail.com>
2022-07-12 17:41:47 -0400

    Update router for climbing sessions and training sessions

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -31,11 +31,19 @@ const {
 } = require('../../controllers/projectController');
 
 const {
-    
+    getClimbingSessions,
+    getSingleClimbingSession,
+    updateClimbingSession,
+    deleteClimbingSession,
+    createClimbingSession 
 } = require('../../controllers/climbingSessionController');
 
 const {
-
+    getSingleTrainingSession,
+    getTrainingSessions,
+    updateTrainingSession,
+    createTrainingSession,
+    deleteTrainingSession
 } = require('../../controllers/trainingSessionController');
 //  ====================================================== //
 // User Routes (all work)
@@ -56,13 +64,23 @@ router.route('/:userId/send').post(protect, createSend);
 router.route('/:userId/send/:id').get(protect, getSingleSend).delete(protect, deleteSend).put(protect, updateSend);
 //  ====================================================== //
 // Project Routes from user (all work)
-router.route('/projects').get(protect, getProjects);
+router.route('/project').get(protect, getProjects);
 
-router.route('/:userId/projects').post(protect, createProject);
+router.route('/:userId/project').post(protect, createProject);
 
-router.route('/:userId/projects/:id').get(protect, getSingleProject).delete(protect, deleteProject).put(protect, updateProject);
+router.route('/:userId/project/:id').get(protect, getSingleProject).delete(protect, deleteProject).put(protect, updateProject);
 //  ====================================================== //
 // Climbing sessions from user
+router.route('/climbingsession').get(protect, getClimbingSessions);
 
+router.route('/:userId/climbingsession').post(protect, createClimbingSession);
+
+router.route('/:userId/climbingsession/:id').get(protect, getSingleClimbingSession).delete(protect, deleteClimbingSession).put(protect, updateClimbingSession);
 //  ====================================================== //
+// Training sessions from user
+router.route('/trainingsession').get(protect, getTrainingSessions);
+
+router.route('/:userId/trainingsession').post(protect, createTrainingSession);
+
+router.route('/:userId/trainingsession/:id').get(protect, getSingleTrainingSession).delete(protect, deleteTrainingSession).put(protect, updateTrainingSession);
 module.exports = router;
