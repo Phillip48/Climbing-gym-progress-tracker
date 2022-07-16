9d5a753ea8271a0613179ceb40224d6c8371909c
Phillip48 <phillippereira02@gmail.com>
2022-07-16 10:56:25 -0400

    Small updates and adding notes to the code

--- server/controllers/sendController.js
+++ server/controllers/sendController.js
@@ -48,12 +48,12 @@ const createSend = asyncHandler(async (req, res) => {
         res.status(401)
         throw new Error('User not found')
     }
-
+    // Make sure all the needed fields is there for the send
     if (!req.body.actualGrade || !req.body.totalAttempts || !req.body.feltGrade || !req.body.sent || !req.body.totalSessions) {
         res.status(400)
         throw new Error('Please add the needed fields')
     }
-
+    // Establish the sendObj for later use
     let sendObj = {
         actualGrade: req.body.actualGrade,
         feltGrade: req.body.feltGrade,
@@ -65,11 +65,12 @@ const createSend = asyncHandler(async (req, res) => {
         climbingSession: req.body.climbingSession,
         user: req.user.id,
     }
-
+    // If the user sends a climbing session id
     if (req.body.climbingSession) {
         const findID = await ClimbingSession.findOne({ _id : req.body.climbingSession});
+        // and if the climbing session id is null meaning it doesnt exisit
         if (findID === null){
-            // console.log('Not found!');
+            // change the sendObj to not have a climbingSession key in the variable so it doesnt get posted with an id that doesnt exisit
             sendObj = await Send.create({
                 actualGrade: req.body.actualGrade,
                 feltGrade: req.body.feltGrade,
@@ -89,6 +90,7 @@ const createSend = asyncHandler(async (req, res) => {
             res.status(200).json(updatedUser)
             throw new Error('Send was created however no Climbing Session was found with that ID')
         } else {
+            // if theres a climbing session id and it does exisit it will run this
             sendObj = await Send.create({
                 actualGrade: req.body.actualGrade,
                 feltGrade: req.body.feltGrade,
@@ -110,19 +112,25 @@ const createSend = asyncHandler(async (req, res) => {
             })
             res.status(200).json(updatedSessions)
         }
+        // If there isnt a climbing session id this will run 
+    } else {
+        sendObj = await Send.create({
+            actualGrade: req.body.actualGrade,
+            feltGrade: req.body.feltGrade,
+            notes: req.body.notes,
+            sent: req.body.sent,
+            totalAttempts: req.body.totalAttempts,
+            totalSessions: req.body.totalSessions,
+            videoOrImg: req.body.videoOrImg,
+            user: req.user.id,
+        })
+        const updatedUser = await User.findByIdAndUpdate(
+            { _id: req.user.id },
+            { $addToSet: { sends: sendObj } },
+            { runValidators: true, new: true }
+        );
+        res.status(200).json(updatedUser)
     }
-    // console.log('In the function', sendObj)
-    // sendObj = await Send.create({
-    //     actualGrade: req.body.actualGrade,
-    //     feltGrade: req.body.feltGrade,
-    //     notes: req.body.notes,
-    //     sent: req.body.sent,
-    //     totalAttempts: req.body.totalAttempts,
-    //     totalSessions: req.body.totalSessions,
-    //     videoOrImg: req.body.videoOrImg,
-    //     // climbingSession: req.body.climbingSession,
-    //     user: req.user.id,
-    // })
 })
 
 // update a send
