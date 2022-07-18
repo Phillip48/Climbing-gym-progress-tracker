b7ff6506b572c96e1a2a9cdeaed031c36d020191
Phillip48 <phillippereira02@gmail.com>
2022-07-18 18:06:18 -0400

    Complete refractor of the components folder. Moved code to pages folder and css to the index.css file

--- client/src/pages/404Page.js
+++ client/src/pages/404Page.js
@@ -1,16 +1,12 @@
 import React from "react";
-import NotFound from "../components/404/index.js";
-// import HeaderBanner from '../components/banner/index.js';
-// import ContactMeComp from "../components/contactForm/Index";
-// import Footer from '../components/footer/index';
+
 
 const Homepage = () => {
     return (
         <>
-            {/* <HeaderBanner /> */}
-            <NotFound />
-            {/* <ContactMeComp /> */}
-            {/* <Footer /> */}
+            <div>
+                <h1>Not Found</h1>
+            </div>
         </>
     );
 };
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,12 +1,60 @@
-import React from "react";
-import DashComp from "../components/dashboard/index";
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+// import GoalForm from '../components/GoalForm'
+// import GoalItem from '../components/GoalItem'
+import Spinner from '../components/Spinner'
+import { getSends, reset } from '../features/sends/sendsSlice'
 
-const DashBoardPage = () => {
+function Dashboard() {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const { user } = useSelector((state) => state.auth)
+    const { sends, isLoading, isError, message } = useSelector(
+        (state) => state.sends
+    )
+
+    useEffect(() => {
+        if (isError) {
+            console.log(message)
+        }
+
+        if (!user) {
+            navigate('/login')
+        }
+
+        dispatch(getSends())
+
+        return () => {
+            dispatch(reset())
+        }
+    }, [user, navigate, isError, message, dispatch])
+
+    if (isLoading) {
+        return <Spinner />
+    }
+
     return (
         <>
-            <DashComp />
-        </>
-    );
-};
+            <section className='heading'>
+                {/* <h1>Welcome {user.userName}</h1> */}
+                <p>Dashboard</p>
+            </section>
 
-export default DashBoardPage;
+            {/* <section className='content'>
+                {goals.length > 0 ? (
+                    <div className='goals'>
+                        {goals.map((goal) => (
+                            <GoalItem key={goal._id} goal={goal} />
+                        ))}
+                    </div>
+                ) : (
+                    <h3>You have not set any goals</h3>
+                )}
+            </section> */}
+        </>
+    )
+}
+
+export default Dashboard
